44e72879d3b7fe1cc3f9b829c8fab221fc824238
Kavi Gupta <kavi@globalmbox.com>
2018-04-15 02:33:08 -0700

    add tests.

---
+++ tests.py
@@ -0,0 +1,36 @@
+
+from os.path import expanduser, join
+from os import system
+import unittest
+
+from shell_extensions_python import cd, pwd, ls, mkdir, cat, write, rm
+
+system('rm -r tests/* tests/.*')
+mkdir('tests')
+INITIAL_PWD = join(pwd(), 'tests')
+def reset(fn):
+    def modified(*args):
+        cd(INITIAL_PWD)
+        fn(*args)
+    return modified
+
+class Tests(unittest.TestCase):
+
+    @reset
+    def test_basic_cd(self):
+        initial = pwd()
+        cd()
+        self.assertEqual(expanduser("~"), pwd())
+        cd(1)
+        self.assertEqual(initial, pwd())
+    @reset
+    def test_empty_ls(self):
+        self.assertEqual(ls(), [])
+    @reset
+    def test_add_file(self):
+        write('test.py', 'hi!!!')
+        self.assertEqual(cat('test.py'), 'hi!!!')
+        rm('test.py')
+
+if __name__ == '__main__':
+    unittest.main()
