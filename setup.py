ad0b717e043a406f3c78d6d1ba4c91f3d91f7eec
Kavi Gupta <kavi@globalmbox.com>
2018-04-16 17:53:00 -0700

    add setup information

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='shell-extensions-python',
+      version='0.1',
+      description='A shell in python, both a library and an interactive mode',
+      url='http://github.com/kavigupta/shell-extensions-python',
+      author='Kavi Gupta',
+      author_email='kavi@berkeley.edu',
+      license='GPL 3.0',
+      packages=['shell_extensions_python'],
+      zip_safe=False)
