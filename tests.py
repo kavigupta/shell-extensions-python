410168c0787f655b534d72038b68ad08a5f25e3e
Kavi Gupta <kavi@globalmbox.com>
2018-04-17 16:42:56 -0700

    add some more tests for ls

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 from os.path import expanduser, join
 from os import system
 import unittest
+from random import randint
 
 from shell_extensions_python import cd, pwd, ls, mkdir, cat, write, rm, pload, psave
 
@@ -52,6 +53,37 @@ class Tests(unittest.TestCase):
     @reset
     def test_empty_ls(self):
         self.assertEqual(ls(), [])
+
+    @reset
+    def test_full_path_ls(self):
+        mkdir('path/to')
+        write('path/to/file.txt', 'contents')
+        self.assertEqual(ls('path'), ['to'])
+        self.assertEqual(ls('path', full=True), ['path/to'])
+        cd('path')
+        self.assertEqual(ls('..'), ['path'])
+        self.assertEqual(ls('..', full=True), ['../path'])
+        cd('..')
+        rm('path', remove_recursively=True)
+
+    @reset
+    def test_all_ls(self):
+        write("shown", '')
+        write(".hidden", '')
+        self.assertEqual(ls(), [".hidden", "shown"])
+        self.assertEqual(ls(a=False), ["shown"])
+        rm("shown")
+        rm(".hidden")
+
+    @reset
+    def test_sorted_ls(self):
+        paths = [hex(randint(0, 0x10000000000)) for _ in range(50)]
+        for path in paths:
+            write(path, '')
+        self.assertEqual(sorted(paths), ls())
+        for path in paths:
+            rm(path)
+
     @reset
     def test_add_file(self):
         write('test.py', 'hi!!!')
