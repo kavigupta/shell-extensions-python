a7bd08d65908ba9b5f6080ee0f1dfa9ab813dd0e
Kavi Gupta <kavi@globalmbox.com>
2018-04-17 00:09:16 -0700

    add module docstring

--- shell_extensions_python/__init__.py
+++ shell_extensions_python/__init__.py
@@ -1,3 +1,6 @@
+"""
+A package that is supposed to allow you to use python rather than bash as your shell.
+"""
 
 from .basic_shell_programs import ls, cat, pwd, cd, globs, glob, mkdir, write, rm
 from .shell_pickles import pload, ploads, psaves, psave
