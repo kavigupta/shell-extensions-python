cb5b88b664922453a19e87efd1c1ea7024487b09
Kavi Gupta <kavi@globalmbox.com>
2018-04-18 16:32:59 -0700

    Add option to throw an error if the process called by fails

--- shell_extensions_python/run_shell_commands.py
+++ shell_extensions_python/run_shell_commands.py
@@ -14,18 +14,34 @@ def iterable(value):
     except: # pylint: disable=bare-except
         return False
 
-def r(command, std=False, err=False):
+class ProcessFailedException(RuntimeError):
+    """
+    An exception representing a failed process
+    """
+    pass
+
+def r(command, std=False, err=False, throw=False):
     """
     Run the given command, and optionally gather the stdout and stderr
 
     If command is a string, run it as a shell command, if command is an iterable, run it as a execve command.
+
+    If throw is true, this raises a RuntimeError whenever the result has a nonzero exit code
     """
+    if throw is True:
+        throw = ProcessFailedException
     std_proc = subprocess.PIPE if std else None
     err_proc = subprocess.PIPE if err else None
     if isinstance(command, str):
-        return subprocess.run(command, shell=True, stdout=std_proc, stderr=err_proc)
+        result = subprocess.run(command, shell=True, stdout=std_proc, stderr=err_proc)
     elif iterable(command):
         command = list(command)
         if all(isinstance(x, str) for x in command):
-            return subprocess.run(command, stdout=std_proc, stderr=err_proc)
-    raise RuntimeError("Cannot run %s" % command)
+            result = subprocess.run(command, stdout=std_proc, stderr=err_proc)
+        else:
+            raise RuntimeError("Cannot run %s: it has non-string elements" % command)
+    else:
+        raise RuntimeError("Expected str or some iterable, but got %s" % type(command))
+    if result.returncode != 0 and throw:
+        raise throw("Bad exit code: %s" % result.returncode)
+    return result
