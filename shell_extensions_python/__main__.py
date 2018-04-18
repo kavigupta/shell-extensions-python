4049d7f99b2894d4508321e3c13788dc91ae6291
Kavi Gupta <kavi@globalmbox.com>
2018-04-18 16:42:55 -0700

    Modify the PS1 framework to allow for a more complex PS1 output

--- shell_extensions_python/__main__.py
+++ shell_extensions_python/__main__.py
@@ -1,7 +1,18 @@
 # pylint: disable-all
 
 from shell_extensions_python import *
-PS1.set()
+
+from shell_extensions_python.ps1 import SeriesRenderer, UserRenderer, GitPathRenderer, GitStatusRenderer
+from colors import *
+
+SeriesRenderer(
+    UserRenderer(user_color=yellow_bright),
+    " ",
+    GitPathRenderer(outside_color=orange, repo_color=green_bright, local_color=green),
+    " ",
+    GitStatusRenderer(modified=orange, new=green_bright, deleted=red, staged=blue, behind=red, ahead=green_bright, branch_color=cyan),
+    " $ "
+).set()
 
 from colorama import init
 init()
--- shell_extensions_python/ps1.py
+++ shell_extensions_python/ps1.py
@@ -4,19 +4,149 @@ Several extensions for python to be usable as a bash-style shell.
 
 import sys
 
-from .basic_shell_programs import pwd
-from .path_manipulation import unexpand_user
+from abc import ABCMeta, abstractmethod
 
-class PS1:
-    """
-    A class representing the left prompt
-    """
-    def __str__(self):
-        return unexpand_user(pwd()) + " $ "
+from collections import Counter
 
-    @staticmethod
-    def set():
+import shell_extensions_python.git as git
+from .git import GitStatusCategory
+
+from .colors import reset
+
+
+from .basic_shell_programs import pwd, whoami
+from .run_shell_commands import ProcessFailedException
+
+from .path_manipulation import unexpand_user, basename, dirname
+
+class Renderer(metaclass=ABCMeta):
+    """
+    Base class of PS1 classes
+    """
+    @abstractmethod
+    def render(self):
+        """
+        Renders the current state of the universe as a string with color codes
+        """
+        pass
+    def set(self):
         """
         Set this as the PS1
         """
-        sys.ps1 = PS1()
+        sys.ps1 = self
+    def __str__(self):
+        return self.render()
+
+class GitPathRenderer(Renderer):
+    """
+    A renderer that renders the path with different colors for the git directory
+    """
+    def __init__(self, outside_color, repo_color, local_color):
+        self.outside_color = outside_color
+        self.repo_color = repo_color
+        self.local_color = local_color
+    @staticmethod
+    def current_repository_split():
+        """
+        Return the path of the current repository, split into the parent of the repository,
+            and the repository's exact name
+        """
+        git_directory = git.current_repository()
+        above = unexpand_user(dirname(git_directory))
+        in_directory = basename(git_directory)
+        return above, "/" + in_directory
+    def render(self):
+        try:
+            above, repo = GitPathRenderer.current_repository_split()
+            local = git.relative_path_in_repository()
+            return self.outside_color + above + self.repo_color + repo + self.local_color + local + reset
+        except ProcessFailedException:
+            return self.outside_color + pwd() + reset
+
+class GitStatusRenderer(Renderer):
+    """
+    Renders git's current status in the form
+        branch [-behind tracking, +ahead tracking] <status wrt master>
+    """
+    #pylint: disable=too-many-arguments
+    def __init__(self, modified, new, deleted, staged, behind, ahead, branch_color):
+        self.modified = modified
+        self.new = new
+        self.deleted = deleted
+        self.staged = staged
+        self.behind = behind
+        self.ahead = ahead
+        self.branch_color = branch_color
+    def render_branch(self):
+        """
+        Render the current branch
+        """
+        branch = git.current_branch()
+        return self.branch_color + branch + reset
+    def git_offsets(self):
+        """
+        Render the offsets with respect to the tracking branch
+        """
+        ahead, behind = git.commits_wrt_tracking()
+        newoffs = []
+        if behind:
+            newoffs.append(self.behind + "-" + str(behind))
+        if ahead:
+            newoffs.append(self.ahead + "+" + str(ahead))
+        result = ", ".join(newoffs)
+        if result == "":
+            return ""
+        return reset + "[" + result + reset + "]"
+    def process_status(self, prefix, count):
+        """
+        Colors the given status and count, and places an @ in between
+        """
+        item_color = {
+            GitStatusCategory.modified : self.modified,
+            GitStatusCategory.added : self.new,
+            GitStatusCategory.deleted : self.deleted,
+            GitStatusCategory.staged : self.staged,
+            GitStatusCategory.other : reset
+        }[prefix.category()]
+        return item_color + prefix.status_str() + "@" + str(count) + reset
+    def one_line_status(self):
+        """
+        Prints out a one-line representation of the status of the git repository
+        """
+        prefixes = sorted(Counter(stat for stat, _ in git.status_summary()).items())
+        summary = " ".join(self.process_status(prefix, count) for prefix, count in prefixes)
+        if not summary:
+            return ""
+        return reset + " <" + summary + reset + ">"
+    def render(self):
+        try:
+            return self.render_branch() + " " + self.git_offsets() + self.one_line_status()
+        except ProcessFailedException:
+            return ""
+
+class UserRenderer(Renderer):
+    """
+    Renders the user
+    """
+    def __init__(self, user_color):
+        self.user_color = user_color
+    def render(self):
+        return self.user_color + whoami() + reset
+
+class ConstRenderer(Renderer):
+    """
+    Renders the given element
+    """
+    def __init__(self, element):
+        self.element = element
+    def render(self):
+        return self.element
+
+class SeriesRenderer(Renderer):
+    """
+    Renders its arguments in series
+    """
+    def __init__(self, *renderers):
+        self.renderers = [ConstRenderer(x) if isinstance(x, str) else x for x in renderers]
+    def render(self):
+        return "".join(x.render() for x in self.renderers)
