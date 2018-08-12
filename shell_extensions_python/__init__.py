e7d079c8111514492738a4278a63ba3f2cf4ab9b
Kavi Gupta <kavi@globalmbox.com>
2018-08-12 15:18:00 -0700

    add collect to the main package

--- shell_extensions_python/__init__.py
+++ shell_extensions_python/__init__.py
@@ -7,7 +7,7 @@ from sys import argv
 from . import git
 
 from .basic_shell_programs import ls, cat, pwd, cd, globs, glob, mkdir, write, rm, whoami, CannotRemoveDirectoryError
-from .run_shell_commands import r, s, less, cp, StderrRed
+from .run_shell_commands import r, s, less, cp, StderrRed, Collect
 from .shell_pickles import pload, ploads, psaves, psave
 from .grep import cgrep
 
