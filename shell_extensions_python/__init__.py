06a2503452c25dbec25ac2699e8e80627d34f267
Kavi Gupta <kavi@globalmbox.com>
2018-08-14 15:51:59 -0700

    expose ProcessFailedException

--- shell_extensions_python/__init__.py
+++ shell_extensions_python/__init__.py
@@ -10,7 +10,7 @@ from . import git
 
 from .basic_shell_programs import ls, cat, pwd, cd, globs, glob, mkdir, write, rm, mv, move_to, whoami, \
     CannotRemoveDirectoryError
-from .run_shell_commands import r, s, less, cp, StderrRed, Collect
+from .run_shell_commands import r, s, less, cp, StderrRed, Collect, ProcessFailedException
 from .shell_pickles import pload, ploads, psaves, psave
 from .grep import cgrep
 
