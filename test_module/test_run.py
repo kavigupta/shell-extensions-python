beb228168aaff27ef2c57da9186be6fb0fd81772
Kavi Gupta <kavi@globalmbox.com>
2018-08-14 15:52:10 -0700

    Add more tests for run_shell_commands

--- test_module/test_run.py
+++ test_module/test_run.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from shell_extensions_python import write, ls, r, s, rm
+from shell_extensions_python import write, ls, r, s, rm, Collect, ProcessFailedException
 from shell_extensions_python.run_shell_commands import FD
 
 from .utilities import reset
@@ -14,9 +14,28 @@ class TestRun(unittest.TestCase):
         rm('test')
         rm('test2')
     @reset
+    def test_with_args(self):
+        self.assertEqual('-n with space', r(('echo', '-n with space'), mode=Collect).stdout(single_line=True))
+    @reset
+    def test_invalid_args(self):
+        self.assertRaises(RuntimeError, lambda: r(None))
+        self.assertRaises(RuntimeError, lambda: r([['abc']]))
+    @reset
+    def test_multiple_lines(self):
+        self.assertEqual('2\n3\n', r('echo 2; echo 3', mode=Collect).stdout())
+        self.assertRaises(RuntimeError, lambda: r('echo 2; echo 3', mode=Collect).stdout(single_line=True))
+        self.assertEqual(['2', '3'], r('echo 2; echo 3', mode=Collect).stdout(as_lines=True))
+        self.assertRaises(RuntimeError, lambda: r('echo 2; echo 3', mode=Collect).stdout(as_lines=True, single_line=True))
+    @reset
     def test_exit_code(self):
         self.assertEqual(True, bool(r('true')))
         self.assertEqual(False, bool(r('false')))
+    @reset
+    def test_or_throw(self):
+        r('true').or_throw()
+        r('false').or_throw(throw=False)
+        self.assertRaises(ProcessFailedException, lambda: r('false').or_throw())
+        self.assertRaises(FileNotFoundError, lambda: r('false').or_throw(throw=FileNotFoundError))
     @reset
     def test_return_value(self):
         self.assertEqual("", repr(r('true')))
@@ -24,3 +43,11 @@ class TestRun(unittest.TestCase):
     def test_callback_order(self):
         result = list(s('echo abc >&2; sleep 0.1; echo def'))
         self.assertEqual([(FD.stderr, b'abc\n'), (FD.stdout, b'def\n')], result)
+    @reset
+    def test_get_exitcode(self):
+        command = s('echo abc')
+        self.assertRaises(RuntimeError, lambda: command.exitcode)
+    @reset
+    def test_collect(self):
+        self.assertEqual('abc\n', (s('echo abc') > Collect).stdout())
+        self.assertEqual('abc\n', (s('echo abc').collect(Collect)).stdout())
