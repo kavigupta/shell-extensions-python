4b57ee2266d4e1cdca61bea44d054ffbfbd9c79a
Kavi Gupta <kavi@globalmbox.com>
2018-08-14 16:44:42 -0700

    add tests for ls coloring

--- test_module/test_ls.py
+++ test_module/test_ls.py
@@ -3,7 +3,8 @@ import unittest
 
 from random import randint
 
-from shell_extensions_python import ls, cd, rm, mkdir, write
+from shell_extensions_python import ls, cd, rm, mkdir, write, r
+from shell_extensions_python.interactive import FileType, DisplayPath
 
 from .utilities import reset
 
@@ -38,3 +39,25 @@ class TestLs(unittest.TestCase):
         self.assertEqual(sorted(paths), ls())
         for path in paths:
             rm(path)
+    @reset
+    def test_colors(self):
+        write('file', 'contents')
+        mkdir('folder')
+        write('deleted', 'to be deleted')
+        write('executable', 'to be deleted')
+        r('ln -s file link')
+        r('chmod +x executable')
+        deleted, executable, file, folder, link = ls()
+        rm('deleted')
+        self.assertEqual(FileType.normal_file, deleted.type)
+        self.assertEqual(FileType.normal_file, file.type)
+        self.assertEqual(FileType.executable, executable.type)
+        self.assertEqual(FileType.directory, folder.type)
+        self.assertEqual(FileType.link, link.type)
+        rm('file')
+        rm('folder')
+        rm('executable')
+        r('rm link')
+    @reset
+    def test_missing(self):
+        self.assertEqual(FileType.missing, DisplayPath("nonexistant", ".").type)
