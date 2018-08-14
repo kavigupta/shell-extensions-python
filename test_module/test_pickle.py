f9036cfbed3e8b8df51f52f86599fd23e459ae36
Kavi Gupta <kavi@globalmbox.com>
2018-08-14 15:37:34 -0700

    add more tests for ploads/psaves

--- test_module/test_pickle.py
+++ test_module/test_pickle.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from shell_extensions_python import psave, pload, rm
+from shell_extensions_python import psave, psaves, pload, ploads, rm
 from shell_extensions_python.shell_types import ShellBool
 
 from .utilities import reset
@@ -11,4 +11,17 @@ class TestPickle(unittest.TestCase):
     def test_pickle(self):
         self.assertEqual(ShellBool.true, psave('test.pkl', [1, 2, 3]))
         self.assertEqual(pload('test.pkl'), [1, 2, 3])
+        rm('test.pkl')
+    @reset
+    def test_pickles(self):
+        self.assertEqual(ShellBool.true, psaves('test.pkl', [[1, 2, 3], {4 : 5}]))
+        self.assertEqual([[1, 2, 3], {4 : 5}], ploads('test.pkl'))
+        self.assertEqual([[1, 2, 3]], ploads('test.pkl', limit=1))
+        rm('test.pkl')
+    @reset
+    def test_pload_wrong_size(self):
+        self.assertEqual(ShellBool.true, psaves('test.pkl', []))
+        self.assertRaises(RuntimeError, lambda: pload('test.pkl'))
+        self.assertEqual(ShellBool.true, psaves('test.pkl', [1, 2]))
+        self.assertRaises(RuntimeError, lambda: pload('test.pkl'))
         rm('test.pkl')
